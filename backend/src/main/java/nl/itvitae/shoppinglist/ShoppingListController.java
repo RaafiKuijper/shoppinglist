bdc10f7519dbc1179547a91e78145c00d983a583
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2023-12-15 12:31:23 +0100

    Frontend works

--- backend/src/main/java/nl/itvitae/shoppinglist/ShoppingListController.java
+++ backend/src/main/java/nl/itvitae/shoppinglist/ShoppingListController.java
@@ -2,11 +2,13 @@ package nl.itvitae.shoppinglist;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/shopping-list-items")
 public class ShoppingListController {
     @GetMapping
