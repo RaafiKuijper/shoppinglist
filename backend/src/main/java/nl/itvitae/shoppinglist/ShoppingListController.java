8af82582fe0367e8bc4e6a67ef76480e9caaeb79
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2023-12-15 12:18:33 +0100

    Backend works

---
+++ backend/src/main/java/nl/itvitae/shoppinglist/ShoppingListController.java
@@ -0,0 +1,20 @@
+package nl.itvitae.shoppinglist;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/shopping-list-items")
+public class ShoppingListController {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+            "boter",
+            "kaas",
+            "eieren"
+        );
+    }
+}
